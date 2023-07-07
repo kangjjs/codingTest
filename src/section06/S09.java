bfc3ef78889cd9161a6cb42199d1d2a48e3f6497
kangjjs <emdtod12@naver.com>
2023-07-07 11:21:05 +0900

    뮤직비디오

---
+++ src/section06/S09.java
@@ -0,0 +1,54 @@
+package section06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class S09 {
+
+    public int count(int[] arr, int n) {
+        int cnt = 1, sum = 0;
+
+        for (int x : arr) {
+            if (sum + x > n) {
+                cnt++;
+                sum = x;
+            } else
+                sum += x;
+        }
+
+        return cnt;
+    }
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+
+            if(count(arr,mid)<=m){
+                answer=mid;
+                rt=mid-1;
+            }else
+                lt=mid+1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        S09 T = new S09();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.print(T.solution(n, m, arr));
+    }
+}
