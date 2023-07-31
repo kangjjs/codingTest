9518a58cd3f3499cf1c93a6cf205f457748f8e1b
kangjjs <emdtod12@naver.com>
2023-07-31 12:56:16 +0900

    합이 같은 부분집합

--- src/section08/S01.java
+++ src/section08/S01.java
@@ -1,38 +1,47 @@
 package section08;
 
-import java.util.*;
-class S01 {
-    static String answer="NO";
-    static int n, total=0;
-    boolean flag=false;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class S01 {
+
+    static int n,sum=0;
     static int [] arr;
+    static boolean flag=false;
 
-    public void solution(int L, int sum){
-        if(flag) return;
-        if(sum>total/2) return;
-        if(L==n){
-            if((total-sum)==sum){
-                answer="YES";
+    public void solution(int l,int s){
+        if(flag)
+            return;
+        if(s>sum/2)
+            return;
+        if(l==n){
+            if(sum-s==s)
                 flag=true;
-            }
         }
         else{
-            solution(L+1, sum+arr[L]);
-            solution(L+1, sum);
+            solution(l+1,s+arr[l]);
+            solution(l+1,s);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
-        n=kb.nextInt();
+
+        n =kb.nextInt();
         arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=kb.nextInt();
-            total+=arr[i];
+
+        for(int i=0;i<n;i++){
+            arr[i]= kb.nextInt();
+            sum+=arr[i];
         }
-        T.solution(0, 0);
-        System.out.print(answer);
+
+        Arrays.sort(arr);
+        T.solution(0,0);
+
+        if(flag)
+            System.out.print("YES");
+        else
+            System.out.print("NO");
     }
 }
