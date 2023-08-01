74f065ed1506f7377efc51d0da78eb727a981436
kangjjs <emdtod12@naver.com>
2023-08-01 12:36:57 +0900

    최대점수 구하기

--- src/section08/S03.java
+++ src/section08/S03.java
@@ -4,34 +4,37 @@ import java.util.Scanner;
 
 public class S03 {
 
-    static int n,m,sum=0;
-    static int [] score;
-    static int [] time;
+    static int n,m,answer=Integer.MIN_VALUE;
 
-    public void solution(int k,int t,int s){
+    static int [] time,score;
+
+    public void solution(int l,int t,int s){
         if(t>m)
             return;
-        if(k==n) {
-            sum=Math.max(sum,s);
-        } else{
-            solution(k+1,t+time[k],s+score[k]);
-            solution(k+1,t,s);
+        if(l==n){
+            answer=Math.max(s,answer);
+        }else{
+            solution(l+1,t+time[l],s+score[l]);
+            solution(l+1,t,s);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         S03 T = new S03();
         Scanner kb = new Scanner(System.in);
+
         n=kb.nextInt();
         m=kb.nextInt();
-        score=new int[n];
-        time=new int[n];
+
+        time = new int[n];
+        score = new int[n];
+
         for(int i=0;i<n;i++){
             score[i]=kb.nextInt();
             time[i]=kb.nextInt();
         }
 
         T.solution(0,0,0);
-        System.out.print(sum);
+        System.out.print(answer);
     }
 }
