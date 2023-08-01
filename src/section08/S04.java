eac6daec8de83fad01a04c356be4284a696c29dd
kangjjs <emdtod12@naver.com>
2023-08-01 12:51:49 +0900

    중복순열

--- src/section08/S04.java
+++ src/section08/S04.java
@@ -7,15 +7,16 @@ public class S04 {
     static int n,m;
     static int [] arr;
 
-    public void solution(int k){
-        if(k==m){
-            for(int x: arr)
+    public void solution(int l){
+        if(l==m){
+            for(int x: arr){
                 System.out.print(x+" ");
+            }
             System.out.println();
         }else{
             for(int i=1;i<=n;i++){
-                arr[k]=i;
-                solution(k+1);
+                arr[l]=i;
+                solution(l+1);
             }
         }
     }
@@ -23,8 +24,10 @@ public class S04 {
     public static void main(String[] args) {
         S04 T = new S04();
         Scanner kb = new Scanner(System.in);
+
         n = kb.nextInt();
         m = kb.nextInt();
+
         arr = new int[m];
 
         T.solution(0);
