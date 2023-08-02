4345add7f38df6c1b9caf78f5eccade963ede043
kangjjs <emdtod12@naver.com>
2023-08-02 12:12:00 +0900

    수열 추측하기

--- src/section08/S08.java
+++ src/section08/S08.java
@@ -5,32 +5,33 @@ import java.util.Scanner;
 public class S08 {
 
     static int n,m;
-    static int [] b,p,ch;
-    boolean flag = false;
-    int [][] dy= new int [35][35];
+    static int [] arr,a,ch;
+    boolean flag=false;
 
-    public int C(int n, int r){
-        if(dy[n][r]>0) return dy[n][r];
-        if(n==r || r==0) return 1;
-        else return dy[n][r] = C(n-1,r-1)+ C(n-1,r);
+    public int C(int c,int r){
+        if (c == r || r==0)
+            return 1;
+        else if (r == 1)
+            return c;
+        else
+            return C(c - 1, r - 1) + C(c - 1, r);
     }
 
-    public void solution(int k,int sum){
+    public void solution(int l,int sum){
         if(flag)
             return;
-        if(k==n){
+        if(l==n){
             if(sum==m){
-                for(int x : p){
+                for(int x:a)
                     System.out.print(x+" ");
-                }
                 flag=true;
             }
         }else{
             for(int i=1;i<=n;i++){
                 if(ch[i]==0){
                     ch[i]=1;
-                    p[k]=i;
-                    solution(k+1,sum+p[k]*b[k]);
+                    a[l]=i;
+                    solution(l+1,sum+(a[l]*arr[l]));
                     ch[i]=0;
                 }
             }
@@ -41,15 +42,15 @@ public class S08 {
         S08 T = new S08();
         Scanner kb = new Scanner(System.in);
 
-        n = kb.nextInt();
-        m = kb.nextInt();
+        n=kb.nextInt();
+        m=kb.nextInt();
 
-        b=new int[n];
-        p=new int[n];
+        a=new int[n];
+        arr=new int[n];
         ch=new int[n+1];
 
         for(int i=0;i<n;i++){
-            b[i]=T.C(n-1,i);
+            arr[i]=T.C(n-1,i);
         }
 
         T.solution(0,0);
