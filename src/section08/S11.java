924cebeae5ca7a518a9519c2df9e15cf1789031c
kangjjs <emdtod12@naver.com>
2023-08-05 02:29:53 +0900

    미로의 최단경로

--- src/section08/S11.java
+++ src/section08/S11.java
@@ -4,40 +4,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Point{
+class Point {
     public int x;
     public int y;
-
     Point(int x,int y){
         this.x=x;
         this.y=y;
     }
 }
-
 public class S11 {
 
-    static int [][] board,answer;
+    static int [][] board,dis;
 
-    int [] dx = {1,0,-1,0};
-    int [] dy = {0,1,0,-1};
+    int [] dx ={1,0,-1,0};
+    int [] dy ={0,1,0,-1};
 
-
-    public void solution(int x,int y){
+    public void solution(){
         Queue<Point> queue =new LinkedList<>();
-
-        queue.offer(new Point(x,y));
-        board[x][y]=1;
+        queue.offer(new Point(1,1));
 
         while(!queue.isEmpty()){
-            Point p = queue.poll();
+            Point p=queue.poll();
+
             for(int i=0;i<4;i++){
                 int nx=p.x+dx[i];
                 int ny=p.y+dy[i];
 
-                if(nx>=1&&nx<=7&&ny>=1&&ny<=7&&board[nx][ny]==0){
+                if(nx>=1&nx<=7&&ny>=1&&ny<=7&&board[nx][ny]==0){
                     board[nx][ny]=1;
                     queue.offer(new Point(nx,ny));
-                    answer[nx][ny]=answer[p.x][p.y]+1;
+                    dis[nx][ny]=dis[p.x][p.y]+1;
                 }
             }
         }
@@ -48,7 +44,7 @@ public class S11 {
         Scanner kb = new Scanner(System.in);
 
         board = new int[8][8];
-        answer =new int [8][8];
+        dis = new int[8][8];
 
         for(int i=1;i<=7;i++){
             for(int j=1;j<=7;j++){
@@ -56,12 +52,11 @@ public class S11 {
             }
         }
 
-        T.solution(1,1);
+        T.solution();
 
-        if(answer[7][7]==0){
+        if(dis[7][7]==0){
             System.out.print(-1);
-        }
-
-        System.out.print(answer[7][7]);
+        }else
+            System.out.println(dis[7][7]);
     }
 }
