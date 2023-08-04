49f746f4ffc9e6b186ba6d3b32dc4035020a2343
kangjjs <emdtod12@naver.com>
2023-08-05 01:44:02 +0900

    미로탐색

--- src/section08/S10.java
+++ src/section08/S10.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class S10 {
 
-    static int [][] arr = new int[7][7];
     static int answer=0;
-    int [] dx={1,0,-1,0};
-    int [] dy={0,1,0,-1};
+    static int [][] board=new int[8][8];
+
+    int []dx={1,0,-1,0};
+    int []dy={0,1,0,-1};
 
     public void solution(int x,int y){
-        if(x==6&&y==6){
+        if(x==7&&y==7) {
             answer++;
+        }else {
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-        }else{
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-
-                if(nx>=0&&nx<7&&ny>=0&&ny<7&&arr[nx][ny]==0){
-                    arr[nx][ny]=1;
-                    solution(nx,ny);
-                    arr[nx][ny]=0;
+                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    solution(nx, ny);
+                    board[nx][ny] = 0;
                 }
             }
         }
@@ -31,13 +31,15 @@ public class S10 {
         S10 T = new S10();
         Scanner kb = new Scanner(System.in);
 
-        for(int i=0;i<7;i++){
-            for(int j=0;j<7;j++){
-                arr[i][j]=kb.nextInt();
+        for(int i=1;i<=7;i++){
+            for(int j=1;j<=7;j++){
+                board[i][j]=kb.nextInt();
             }
         }
-        arr[0][0]=1;
-        T.solution(0,0);
+
+        board[1][1]=1;
+
+        T.solution(1,1);
         System.out.print(answer);
     }
 }
