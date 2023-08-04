47b238a1271f61c9b3523ba76a190d2b6a35fcf3
kangjjs <emdtod12@naver.com>
2023-08-05 05:31:17 +0900

    피자배달거리

--- src/section08/S15.java
+++ src/section08/S15.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 
 public class S15 {
 
-    static int n,m,len,answer=Integer.MAX_VALUE;
-    static int [] c;
-    static ArrayList<Point> hs,pz;
+    static int n, m, len,answer = Integer.MAX_VALUE;
+    static ArrayList<Point> pz, hm;
+    static int[] arr;
 
-    public void solution(int l,int s){
-        if(l==m){
-            int sum=0;
+    public void solution(int l,int s) {
+        if (l == m) {
+            int sum = 0;
 
-            for(Point p:hs){
-                int dis=Integer.MAX_VALUE;
-                for(int i:c){
-                    dis=Math.min(dis,Math.abs(p.x-pz.get(i).x)+Math.abs(p.y-pz.get(i).y));
+            for (Point h : hm) {
+                int dis = Integer.MAX_VALUE;
+                for (int i : arr) {
+                    dis = Math.min(dis, (Math.abs(h.x - pz.get(i).x) + Math.abs(h.y - pz.get(i).y)));
                 }
-                sum+=dis;
+                sum += dis;
             }
-            answer=Math.min(sum,answer);
-        }else{
-            for(int i=s;i<len;i++){
-                c[l]=i;
-                solution(l+1,i+1);
+
+            answer = Math.min(sum, answer);
+        } else {
+            for (int i = s; i < len; i++) {
+                arr[l] = i;
+                solution(l + 1,s+1);
             }
         }
     }
@@ -33,25 +34,26 @@ public class S15 {
         S15 T = new S15();
         Scanner kb = new Scanner(System.in);
 
-        n=kb.nextInt();
-        m=kb.nextInt();
+        n = kb.nextInt();
+        m = kb.nextInt();
+        arr = new int[m];
+        pz = new ArrayList<>();
+        hm = new ArrayList<>();
 
-        hs=new ArrayList<>();
-        pz=new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = kb.nextInt();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                int tmp=kb.nextInt();
-
-                if(tmp==1)
-                    hs.add(new Point(i,j));
-                else if(tmp==2)
-                    pz.add(new Point(i,j));
+                if (tmp == 1) {
+                    hm.add(new Point(i, j));
+                } else if (tmp == 2) {
+                    pz.add(new Point(i, j));
+                }
             }
         }
 
-        len = pz.size();
-        c=new int[m];
+        len=pz.size();
+
         T.solution(0,0);
         System.out.print(answer);
     }
