cd3367902ab47e83e0ec3ee56f94bd4c0431e2cb
kangjjs <emdtod12@naver.com>
2023-08-05 04:43:32 +0900

    섬나라 아일랜드(bfs)

--- src/section08/S14.java
+++ src/section08/S14.java
@@ -10,20 +10,23 @@ public class S14 {
     static int[][] board;
 
     int[] dx = {1, 1, 1, 0, 0, -1, -1, -1};
-    int[] dy = {1, 0, -1, 1, -1, -1, 0, 1};
+    int[] dy = {1, 0, -1, 1, -1, 1, 0, -1};
     static Queue<Point> queue = new LinkedList<>();
 
     public void bfs(int x, int y) {
+
         queue.offer(new Point(x, y));
+
         while (!queue.isEmpty()) {
             Point p = queue.poll();
+
             for (int i = 0; i < 8; i++) {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
 
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
-                    board[nx][ny] = 0;
-                    queue.offer(new Point(nx, ny));
+                    queue.offer(new Point(nx,ny));
+                    board[nx][ny]=0;
                 }
             }
         }
@@ -32,10 +35,10 @@ public class S14 {
     public void solution() {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==1){
+                if(board[i][j]==1) {
                     answer++;
                     board[i][j]=0;
-                    bfs(i,j);
+                    bfs(i, j);
                 }
             }
         }
