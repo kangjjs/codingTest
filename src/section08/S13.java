63228b54431bbb33e4034ce49e0c3b2bbd5e8523
kangjjs <emdtod12@naver.com>
2023-08-05 04:35:16 +0900

    섬나라 아일랜드(dfs)

--- src/section08/S13.java
+++ src/section08/S13.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class S13 {
 
-    static int n, answer = 0;
-    static int[][] board;
-
+    static int n,answer=0;
+    static int [][] board;
     static int[] dx={-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy={0, 1, 1, 1, 0, -1, -1, -1};
 
-    public void dfs(int x, int y) {
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
-                board[nx][ny] = 0;
-                dfs(nx, ny);
+    public void dfs(int x,int y){
+        for(int i=0;i<8;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+
+            if(nx>=0&&nx<n&&ny>=0&&ny<n&&board[nx][ny]==1){
+                board[nx][ny]=0;
+                dfs(nx,ny);
             }
         }
     }
 
     public void solution() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1){
                     answer++;
-                    board[i][j] = 0;
-                    dfs(i, j);
+                    board[i][j]=0;
+                    dfs(i,j);
                 }
             }
         }
@@ -39,11 +39,11 @@ public class S13 {
 
         n = kb.nextInt();
 
-        board = new int[n][n];
+        board=new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = kb.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]=kb.nextInt();
             }
         }
 
