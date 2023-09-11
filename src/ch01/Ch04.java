bd0b4d2c74bb7009b3a2964986a04242fd76f941
kangjjs <emdtod12@naver.com>
2023-09-11 09:58:44 +0900

    좌석 번호

--- src/ch01/Ch04.java
+++ src/ch01/Ch04.java
@@ -1,39 +1,45 @@
 package ch01;
-import java.util.*;
+
+import java.util.Arrays;
 
 public class Ch04 {
-        public int[] solution(int c, int r, int k) {
-            int[] answer = new int[2];
+    public int[] solution(int c, int r, int k) {
+        int[] answer = new int[2];
 
-            if(k>c*r){
-                return new int[] {0,0};
+        if (k > c * r)
+            return new int[]{0, 0};
+
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        int[][] board = new int[c][r];
+
+        int x = 0, y = 0, count = 1, i = 0;
+
+        while (count < k) {
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= c || ny < 0 || ny >= r || board[nx][ny] > 0) {
+                i = (i + 1) % 4;
+                continue;
             }
 
-            int[][] seat=new int[c][r];
-            int [] dx={0,1,0,-1};
-            int [] dy={1,0,-1,0};
-            int count=1,x=0,y=0,d=0;
+            board[x][y] = count;
+            count++;
+            x = nx;
+            y = ny;
 
-            while(count<k){
-                int nx=x+dx[d];
-                int ny=y+dy[d];
-                if(nx<0||nx>=c||ny<0||ny>=r||seat[nx][ny]==1){
-                    d=(d+1)%4;
-                    continue;
-                }
-                seat[x][y]=1;
-                count++;
-                x=nx;
-                y=ny;
-            }
-
-            answer[0]=x+1;
-            answer[1]=y+1;
-
-            return answer;
         }
 
-    public static void main(String[] args){
+        answer[0] = x + 1;
+        answer[1] = y + 1;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
         Ch04 T = new Ch04();
         System.out.println(Arrays.toString(T.solution(6, 5, 12)));
         System.out.println(Arrays.toString(T.solution(6, 5, 20)));
