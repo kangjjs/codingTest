64b28a9339d1610b0d3cf6a3559d036fe916b891
kangjjs <emdtod12@naver.com>
2023-09-12 17:56:10 +0900

    회의실 만남

--- src/ch01/Ch08.java
+++ src/ch01/Ch08.java
@@ -1,32 +1,40 @@
 package ch01;
 
 import java.util.*;
-class Ch08 {
-    public int[] solution(int[] enter, int[] exit){
-        int n=enter.length;
-        int [] enterIndex=new int[n];
 
-        for(int i=0;i<n;i++){
-            enterIndex[enter[i]-1]=i;
+class Ch08 {
+    public int[] solution(int[] enter, int[] exit) {
+        int[] answer = {};
+        int n = enter.length;
+
+        for (int i = 0; i < n; i++) {
+            enter[i]--;
+            exit[i]--;
         }
 
-        int [] enterTime = new int[n];
-        int [] exitTime = new int[n];
-        int count=0;
+        int[] enterIdx = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            enterIdx[enter[i]] = i;
+        }
+
+        int[] enterT = new int[n];
+        int[] exitT = new int[n];
+
+        int cnt=0;
 
         for(int i=0,j=0;i<n;i++){
-            while(j<n&&j<=enterIndex[exit[i]-1]){
-                enterTime[enter[j]-1]=count++;
+            while(j<n && j <= enterIdx[exit[i]]){
+                enterT[enter[j]]=cnt++;
                 j++;
             }
-            exitTime[exit[i]-1]=count++;
-        }
 
-        int [] answer =new int[n];
+            exitT[exit[i]] = cnt++;
+        }
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(!(exitTime[i]<enterTime[j]||exitTime[j]<enterTime[i])){
+                if(!(exitT[i]<enterT[j] || exitT[j] < enterT[i])){
                     answer[i]++;
                     answer[j]++;
                 }
@@ -36,9 +44,9 @@ class Ch08 {
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Ch08 T = new Ch08();
-        System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3})));
+//        System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 5, 3, 4}, new int[]{2, 3, 1, 4, 5})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 3, 2, 4, 5, 7, 6, 8}, new int[]{2, 3, 5, 6, 1, 4, 8, 7})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 4, 7, 2, 3, 5, 6}, new int[]{5, 2, 6, 1, 7, 3, 4})));
