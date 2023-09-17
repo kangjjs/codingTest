21d2f0ae95eeb545dd0d32ae2a77c6b0168303c5
kangjjs <emdtod12@naver.com>
2023-09-17 18:40:34 +0900

    음수가 있는 부분수열

--- src/ch02/Ch04.java
+++ src/ch02/Ch04.java
@@ -1,26 +1,27 @@
 package ch02;
 
-import java.util.*;
+import java.util.HashMap;
 
 class Ch04 {
-    public int solution(int[] nums, int m) {
+    public int solution(int[] nums, int m){
         int answer = 0;
 
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        hm.put(0, 1);
-        int sum = 0;
+        int sum=0;
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        hm.put(0,1);
 
-        for (int x : nums) {
-            sum += x;
-            if (hm.containsKey(sum - m))
-                answer += hm.get(sum - m);
-            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+        for(int x:nums){
+            sum+=x;
+
+            if(hm.containsKey(sum-m))
+                answer+=hm.get(sum-m);
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
         }
 
         return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Ch04 T = new Ch04();
         System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
         System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
