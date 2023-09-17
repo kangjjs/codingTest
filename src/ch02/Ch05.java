7a1e44b27fc15c87c09b3cdd37e3d00daf5aaf98
kangjjs <emdtod12@naver.com>
2023-09-17 19:57:08 +0900

    회장 선거

--- src/ch02/Ch05.java
+++ src/ch02/Ch05.java
@@ -1,46 +1,51 @@
 package ch02;
 
 import java.util.*;
-class Ch05 {
-    public String solution(String[] votes, int k){
+
+class Solution {
+    public String solution(String[] votes, int k) {
         String answer = " ";
-        HashMap<String, HashSet<String>> voteHash = new HashMap<>();
+
+        HashMap<String, HashSet<String>> hm = new HashMap<>();
         HashMap<String, Integer> candidate = new HashMap<>();
         HashMap<String, Integer> present = new HashMap<>();
-        int max=Integer.MIN_VALUE;
 
         for (String vote : votes) {
             String a = vote.split(" ")[0];
             String b = vote.split(" ")[1];
-            voteHash.putIfAbsent(a,new HashSet<String>());
-            voteHash.get(a).add(b);
-            candidate.put(b,candidate.getOrDefault(b,0)+1);
+
+            hm.putIfAbsent(a, new HashSet<String>());
+            hm.get(a).add(b);
+            candidate.put(b, candidate.getOrDefault(b, 0) + 1);
         }
 
-        for(String s: voteHash.keySet()){
-            int count=0;
-            for(String x:voteHash.get(s)){
-                if(candidate.get(x)>=k)
-                    count++;
+        int max = Integer.MIN_VALUE;
+
+        for (String a : hm.keySet()) {
+            int cnt = 0;
+
+            for (String b : hm.get(a)) {
+                if (candidate.get(b) >= k)
+                    cnt++;
             }
-
-            present.put(s,count);
-            max=Math.max(max,count);
+            present.put(a, cnt);
+            max = Math.max(max, cnt);
         }
 
-        ArrayList<String> tmp=new ArrayList<>();
-        for(String name: present.keySet()){
+        ArrayList<String> tmp =new ArrayList<>();
+        for(String name : present.keySet()){
             if(present.get(name)==max)
                 tmp.add(name);
         }
 
         tmp.sort(String::compareTo);
-        answer = tmp.get(0);
+        answer=tmp.get(0);
+
         return answer;
     }
 
-    public static void main(String[] args){
-        Ch05 T = new Ch05();
+    public static void main(String[] args) {
+        Solution T = new Solution();
         System.out.println(T.solution(new String[]{"john tom", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
         System.out.println(T.solution(new String[]{"john tom", "park luis", "john luis", "luis tom", "park tom", "luis john", "luis park", "park john", "john park", "tom john", "tom park", "tom luis"}, 2));
         System.out.println(T.solution(new String[]{"cody tom", "john tom", "cody luis", "daniel luis", "john luis", "luis tom", "daniel tom", "luis john"}, 2));
