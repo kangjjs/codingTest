401a8dda8cd7bad3b643253882a79dcb19088366
kangjjs <emdtod12@naver.com>
2023-10-02 19:26:15 +0900

    피부과

--- src/ch03/Ch04.java
+++ src/ch03/Ch04.java
@@ -1,49 +1,49 @@
 package ch03;
 
-import java.time.LocalTime;
-import java.util.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Ch04 {
 
     public int getTime(String time){
-        int h = Integer.parseInt(time.split(":")[0]);
-        int m = Integer.parseInt(time.split(":")[1]);
+        int h= Integer.parseInt(time.split(":")[0]);
+        int m= Integer.parseInt(time.split(":")[1]);
 
         return h*60+m;
     }
 
     public int solution(int[] laser, String[] enter){
         int answer = 0;
-
-        int n = enter.length;
-        int [][] medical =new int[n][2];
+        int n= enter.length;
+        int [][] inList = new int[n][2];
 
         for(int i=0;i<n;i++){
-            int time =getTime(enter[i].split(" ")[0]);
-            int order = Integer.parseInt(enter[i].split(" ")[1]);
-            medical[i][0]=time;
-            medical[i][1]=order;
+            int a = getTime(enter[i].split(" ")[0]);
+            int b = Integer.parseInt(enter[i].split(" ")[1]);
+            inList[i][0]=a;
+            inList[i][1]=b;
         }
 
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(medical[0][1]);
-        int ft = medical[0][0];
+        Queue<Integer> queue =new LinkedList<>();
+        queue.offer(inList[0][1]);
+        int fT=inList[0][0];
         int pos=1;
 
-        for(int t =ft; t<=1200;t++){
-            if(pos<n&& t==medical[pos][0]){
-                if(Q.isEmpty() && t>ft){
-                    ft=t;
-                }
-                Q.offer(medical[pos][1]);
+        for(int t=fT;t<=1200;t++){
+            if(pos<n && t == inList[pos][0]){
+                if(queue.isEmpty() && inList[pos][0] > fT)
+                    fT = inList[pos][0];
+                queue.offer(inList[pos][1]);
                 pos++;
             }
 
-            if(t==ft && !Q.isEmpty()){
-                int index = Q.poll();
-                ft +=laser[index];
+            if(t == fT && !queue.isEmpty()){
+                int idx = queue.poll();
+                fT +=laser[idx];
             }
 
-            answer = Math.max(answer,Q.size());
+            answer = Math.max(answer,queue.size());
         }
 
         return answer;
@@ -52,7 +52,7 @@ class Ch04 {
     public static void main(String[] args){
         Ch04 T = new Ch04();
         System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "11:10 2"}));
-        System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
-        System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
+//        System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:23 0", "10:40 3", "10:42 2", "10:52 3", "15:10 0", "15:20 3", "15:22 1", "15:23 0", "15:25 0"}));
+//        System.out.println(T.solution(new int[]{30, 20, 25, 15}, new String[]{"10:20 1", "10:40 1", "11:00 1", "11:20 1", "11:40 1"}));
     }
 }
