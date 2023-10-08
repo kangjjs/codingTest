927c0790c9669b316242093a12221fa51a3d6fcd
kangjjs <emdtod12@naver.com>
2023-10-08 15:58:23 +0900

    겹쳐진 압축 해제

--- src/ch03/Ch02.java
+++ src/ch03/Ch02.java
@@ -3,40 +3,49 @@ package ch03;
 import java.util.Stack;
 
 class Ch02 {
-    public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        Stack<String> st = new Stack<>();
-        for (Character x : s.toCharArray()) {
-            if (x == ')') {
-                String tmp = "";
-                while (!st.empty()) {
-                    String c = st.pop();
-                    if (c.equals("(")) {
-                        StringBuilder num = new StringBuilder();
-                        while (!st.empty() && Character.isDigit(st.peek().charAt(0))) {
-                            num.append(st.pop());
+    public String solution(String s){
+        String answer = "";
+
+        Stack<String> st =new Stack<>();
+
+        for(Character x:s.toCharArray()) {
+            if(x==')'){
+                String tmp="";
+                while(!st.empty()){
+                    String c= st.pop();
+                    if(c.equals("(")){
+                        String res="";
+                        String num="";
+
+                        while(!st.empty()&&Character.isDigit(st.peek().charAt(0))){
+                            num+=st.pop();
                         }
-                        StringBuilder res = new StringBuilder();
-                        int cnt = 0;
-                        if (num.toString().equals(""))
-                            cnt = 1;
+
+                        int cnt=0;
+                        if(num.equals(""))
+                            cnt=1;
                         else
-                            cnt = Integer.parseInt(num.toString());
-                        for (int i = 0; i < cnt; i++)
-                            res.append(tmp);
-                        st.push(res.toString());
+                            cnt=Integer.parseInt(num);
+                        for(int i=0;i<cnt;i++){
+                            res+=tmp;
+                        }
+                        st.push(res);
                         break;
                     }
-                    tmp = c + tmp;
+                    tmp = c+ tmp;
                 }
-            } else st.push(String.valueOf(x));
+            }else
+                st.push(String.valueOf(x));
         }
-        for (String x : st)
-            answer.append(x);
-        return answer.toString();
+
+        for(String x:st){
+            answer+=x;
+        }
+
+        return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Ch02 T = new Ch02();
         System.out.println(T.solution("3(a2(b))ef"));
         System.out.println(T.solution("2(ab)k3(bc)"));
