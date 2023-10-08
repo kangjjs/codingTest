51358a4eabeb9051d2047429123788da2ba961f2
kangjjs <emdtod12@naver.com>
2023-10-08 16:37:40 +0900

    문자 찾기

--- src/section01/S01.java
+++ src/section01/S01.java
@@ -1,23 +1,27 @@
 package section01;
 
-import java.util.Scanner;
-
-public class S01 {
+import java.util.*;
+class S01 {
     public int solution(String s,char c){
-        int answer =0;
 
-        for(Character character: s.toCharArray()){
-            if(c==character)
+        int answer=0;
+
+        for(Character x: s.toCharArray()){
+            if(c==x)
                 answer++;
         }
+
         return answer;
     }
 
     public static void main(String[] args){
-        S01 m =new S01();
-        Scanner in=new Scanner(System.in);
-        String str = in.next().toLowerCase();
-        char c = in.next().toLowerCase().charAt(0);
-        System.out.print(m.solution(str,c));
+        S01 T = new S01();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        char c =kb.next().charAt(0);
+        str=str.toLowerCase();
+        c=Character.toLowerCase(c);
+
+        System.out.print(T.solution(str,c));
     }
 }
