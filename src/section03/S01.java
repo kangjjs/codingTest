4955ccad2102c2d1c0e681bf36a5a0f588b51fa4
kangjjs <emdtod12@naver.com>
2023-10-10 12:24:32 +0900

    두 배열 합치기

--- src/section03/S01.java
+++ src/section03/S01.java
@@ -1,44 +1,49 @@
 package section03;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
+class S01 {
 
-public class S01 {
-
-    public ArrayList<Integer> solution(int []arr1, int []arr2){
+    public ArrayList<Integer> solution(int [] arr1, int [] arr2){
 
         ArrayList<Integer> answer =new ArrayList<>();
 
-        for(int x:arr1){
-            answer.add(x);
+        int i=0,j=0;
+
+        while(i<arr1.length&&j<arr2.length){
+            if(arr1[i]>arr2[j]){
+                answer.add(arr2[j++]);
+            }else{
+                answer.add(arr1[i++]);
+            }
         }
 
-        for(int x:arr2){
-            answer.add(x);
-        }
-
-        Collections.sort(answer);
+        while(i<arr1.length)
+            answer.add(arr1[i++]);
+        while(j<arr2.length)
+            answer.add(arr2[j++]);
 
         return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int [] arr1 = new int[n];
+
+        int n=kb.nextInt();
+        int [] arr1 =new int[n];
+
         for(int i=0;i<n;i++){
             arr1[i]=kb.nextInt();
         }
-        int m=kb.nextInt();
+
+        int m= kb.nextInt();
         int [] arr2 =new int[m];
+
         for(int i=0;i<m;i++){
             arr2[i]=kb.nextInt();
         }
 
-        for(int x:T.solution(arr1,arr2)){
+        for(int x : T.solution(arr1,arr2))
             System.out.print(x+" ");
-        }
     }
 }
