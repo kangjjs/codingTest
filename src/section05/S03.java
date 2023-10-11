6eecf06ea83e67830c321f34a123e1224b8c78a5
kangjjs <emdtod12@naver.com>
2023-10-11 15:59:37 +0900

    크레인 인형뽑기

---
+++ src/section05/S03.java
@@ -0,0 +1,52 @@
+package section05;
+
+import java.util.*;
+
+class S03 {
+
+    public int solution(int [][] arr1,int [] arr2) {
+        int answer=0;
+
+        Stack<Integer> stack =new Stack<>();
+
+        for(int x:arr2){
+            for(int i=0;i<arr1.length;i++){
+                if(arr1[i][x-1]!=0){
+                    int k=arr1[i][x-1];
+                    arr1[i][x-1]=0;
+
+                    if(!stack.isEmpty()&&stack.peek()==k){
+                        stack.pop();
+                        answer+=2;
+                    }else
+                        stack.push(k);
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        S03 T = new S03();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int [][] arr1 = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr1[i][j]=kb.nextInt();
+            }
+        }
+
+        int m = kb.nextInt();
+        int [] arr2 = new int[m];
+
+        for(int i=0;i<m;i++)
+            arr2[i]=kb.nextInt();
+
+        System.out.print(T.solution(arr1,arr2));
+    }
+}
