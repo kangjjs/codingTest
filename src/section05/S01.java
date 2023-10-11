27f3cd34e706cf9780a24a2c458a25de53121c9a
kangjjs <emdtod12@naver.com>
2023-10-11 14:53:51 +0900

    올바른 괄호

--- src/section05/S01.java
+++ src/section05/S01.java
@@ -1,35 +1,37 @@
 package section05;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
-public class S01 {
+class S01 {
 
-    public String solution(String str){
+    public String solution(String str) {
+        String answer="NO";
+
         Stack<Character> stack =new Stack<>();
 
         for(char x:str.toCharArray()){
             if(x=='(')
                 stack.push(x);
-            else{
+            else {
                 if(stack.isEmpty())
                     return "NO";
                 stack.pop();
             }
         }
 
-        if(!stack.isEmpty()){
-            return "NO";
-        }
+        if(stack.isEmpty())
+            return "YES";
 
-        return "YES";
+        return answer;
     }
 
     public static void main(String[] args) {
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+
+        String str= kb.next();
 
         System.out.print(T.solution(str));
     }
 }
+
