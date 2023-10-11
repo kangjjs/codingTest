e01f0511353cdf404570baa2090eeb802f020a32
kangjjs <emdtod12@naver.com>
2023-10-11 16:30:36 +0900

    후위식 연산

---
+++ src/section05/S04.java
@@ -0,0 +1,44 @@
+package section05;
+
+import java.util.*;
+
+class S04 {
+
+    public int solution(String str) {
+
+        int answer=0;
+
+        Stack<Integer> stack =new Stack<>();
+
+        for(Character x: str.toCharArray()){
+            if(Character.isDigit(x))
+                stack.push(x-48);
+            else{
+                int rt=stack.pop();
+                int lt=stack.pop();
+
+                if(x=='+')
+                    stack.push(lt+rt);
+                else if(x=='-')
+                    stack.push(lt-rt);
+                else if(x=='/')
+                    stack.push(lt/rt);
+                else if(x=='*')
+                    stack.push(lt*rt);
+            }
+        }
+
+        answer=stack.get(0);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        S04 T = new S04();
+        Scanner kb = new Scanner(System.in);
+
+        String str= kb.next();
+
+        System.out.print(T.solution(str));
+    }
+}
