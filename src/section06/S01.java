70d027b59837f3e57d523868494a7cac034bc52b
kangjjs <emdtod12@naver.com>
2023-10-12 15:49:12 +0900

    선택 정렬

--- src/section06/S01.java
+++ src/section06/S01.java
@@ -1,23 +1,19 @@
 package section06;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class S01 {
+class S01 {
 
-    public int [] solution(int n,int [] arr){
+    public int [] solution(int n,int []arr) {
 
         for(int i=0;i<n;i++){
-            int idx=i;
-
-
             for(int j=i+1;j<n;j++){
-                if(arr[idx]>arr[j]){
-                    idx=j;
-                }
+               if(arr[i]> arr[j]){
+                   int tmp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = tmp;
+               }
             }
-           int tmp=arr[i];
-            arr[i]=arr[idx];
-            arr[idx]=tmp;
         }
 
         return arr;
@@ -26,14 +22,15 @@ public class S01 {
     public static void main(String[] args) {
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
-        int n= kb.nextInt();
+
+        int n = kb.nextInt();
         int [] arr =new int[n];
 
         for(int i=0;i<n;i++){
             arr[i]=kb.nextInt();
         }
 
-        for(int x:T.solution(n,arr)){
+        for(int x : T.solution(n,arr)){
             System.out.print(x+" ");
         }
     }
