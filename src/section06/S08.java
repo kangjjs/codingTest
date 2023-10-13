40633c595c0778cc1e8f001548b8f18fc15690f6
kangjjs <emdtod12@naver.com>
2023-10-13 16:45:50 +0900

    이분검색

---
+++ src/section06/S08.java
@@ -0,0 +1,46 @@
+package section06;
+
+import java.util.*;
+
+class S08 {
+
+    public int solution(int n,int m, int[] arr) {
+
+        int answer = 0;
+
+        Arrays.sort(arr);
+
+        int lt=0,rt=n-1;
+
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+
+            if(arr[mid]==m){
+                answer=mid+1;
+                break;
+            }
+
+            if(arr[mid]>m)
+                rt=mid-1;
+            else
+                lt=mid+1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        S08 T = new S08();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(n,m,arr));
+    }
+}
