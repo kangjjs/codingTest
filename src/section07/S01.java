1ecec70e72557507782b56f857ea5e74a471a154
kangjjs <emdtod12@naver.com>
2023-10-14 16:10:18 +0900

    재귀함수

--- src/section07/S01.java
+++ src/section07/S01.java
@@ -1,27 +1,25 @@
 package section07;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class S01 {
+class S01 {
 
-    public void recursion(int n){
-        if(n==0)
-            return;
+    public void solution(int n) {
+        if(n<=1)
+            System.out.print(1+" ");
         else{
-            recursion(n-1);
+            solution(n-1);
             System.out.print(n+" ");
         }
-    }
-
-    public void solution(int n){
-        recursion(n);
     }
 
     public static void main(String[] args) {
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
+
         int n = kb.nextInt();
 
         T.solution(n);
     }
 }
+
