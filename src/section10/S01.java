0f1652f70ed7df9924fbb806f3edf069c1ecaca1
kangjjs <emdtod12@naver.com>
2023-10-18 15:06:06 +0900

    계단 오르기

--- src/section10/S01.java
+++ src/section10/S01.java
@@ -1,26 +1,26 @@
 package section10;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class S01 {
+class S01 {
 
-    static int [] dy;
+    public int solution(int n,int [] arr) {
+        for(int i=3;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
 
-    public int solution(int n){
-        dy[1]=1;
-        dy[2]=2;
-        for(int i=3;i<=n;i++)
-            dy[i]=dy[i-1]+dy[i-2];
-        return dy[n];
+        return arr[n];
     }
+
 
     public static void main(String[] args) {
         S01 T = new S01();
         Scanner kb = new Scanner(System.in);
 
         int n = kb.nextInt();
-        dy=new int[n+1];
-
-        System.out.println(T.solution(n));
+        int [] arr = new int[n+1];
+        arr[1]=1;
+        arr[2]=2;
+        System.out.print(T.solution(n,arr));
     }
 }
