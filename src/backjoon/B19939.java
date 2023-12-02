af58204e92ed49dff829e166b036f48c5f2889d6
kangjjs <emdtod12@naver.com>
2023-12-02 20:19:16 +0900

    백준 19939번:박 터뜨리기

---
+++ src/backjoon/B19939.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B19939 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < K + 1; i++) {
+            N -= i;
+            if (N < 0)
+                break;
+        }
+
+        if (N >= 0) {
+            if(N%K>0)
+                sb.append(K);
+            else
+                sb.append(K-1);
+        } else {
+            sb.append(-1);
+        }
+
+        System.out.println(sb);
+    }
+}
