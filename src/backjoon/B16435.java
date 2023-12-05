67c4b93eab899c6e4bea4e1a4e9d620d3cc24c5d
kangjjs <emdtod12@naver.com>
2023-12-05 12:10:41 +0900

    백준 16435번:스네이크버드

---
+++ src/backjoon/B16435.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B16435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[] fruits =new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            fruits[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(fruits);
+
+        for(int x:fruits){
+            if(x>L)
+                break;
+            else{
+                L++;
+            }
+        }
+
+        System.out.println(L);
+    }
+}
