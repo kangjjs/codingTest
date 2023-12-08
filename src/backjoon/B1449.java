08cf1a01d4afdf7609fed85764be2c49081def8c
kangjjs <emdtod12@naver.com>
2023-12-08 16:01:14 +0900

    백준 1449번:수리공 항승

---
+++ src/backjoon/B1449.java
@@ -0,0 +1,38 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1449 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int ans = 1;
+        double s = arr[0] - 0.5;
+
+        for (int i = 0; i < N; i++) {
+            if (s + L < arr[i]) {
+                ans++;
+                s = arr[i] - 0.5;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
