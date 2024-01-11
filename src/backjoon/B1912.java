2365c22836f930c5d1ba3b11b9327ba100ed6d6e
kangjjs <emdtod12@naver.com>
2024-01-11 09:03:51 +0900

    백준 1912번:연속합

---
+++ src/backjoon/B1912.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0]=arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i]=Math.max(arr[i],dp[i-1]+arr[i]);
+        }
+
+        Arrays.sort(dp);
+        System.out.println(dp[n-1]);
+    }
+}
