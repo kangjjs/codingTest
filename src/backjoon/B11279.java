a360c2b747168ac2464934c29e399959c4e94394
kangjjs <emdtod12@naver.com>
2024-02-28 12:09:57 +0900

    백준 11279번:최대 힙

---
+++ src/backjoon/B11279.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class B11279 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb =new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                if(!queue.isEmpty()) {
+                    sb.append(queue.poll()).append("\n");
+                }else
+                    sb.append("0").append("\n");
+            }else{
+                queue.offer(x);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
