063956575038c09c9d44851a36bfe31a1074884e
kangjjs <emdtod12@naver.com>
2024-03-03 21:59:40 +0900

    사다리타기

--- src/section01/S01.java
+++ src/section01/S01.java
@@ -1,27 +1,31 @@
 package section01;
 
-import java.util.*;
-class S01 {
-    public int solution(String s,char c){
+import java.util.Arrays;
 
-        int answer=0;
+public class S01 {
+    public char[] solution(int n, int[][] ladder) {
+        char[] answer = new char[n];
 
-        for(Character x: s.toCharArray()){
-            if(c==x)
-                answer++;
+        for (int i = 0; i < n; i++) {
+            answer[i] = (char) ('A' + i);
+        }
+
+        for (int[] ints : ladder) {
+            for (int x : ints) {
+                char tmp = answer[x - 1];
+                answer[x - 1] = answer[x];
+                answer[x] = tmp;
+            }
         }
 
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         S01 T = new S01();
-        Scanner kb = new Scanner(System.in);
-        String str=kb.next();
-        char c =kb.next().charAt(0);
-        str=str.toLowerCase();
-        c=Character.toLowerCase(c);
-
-        System.out.print(T.solution(str,c));
+        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
     }
 }
