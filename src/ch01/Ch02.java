9ba702cf8d3f9a6df4f61a1b15cca62946ec6576
kangjjs <emdtod12@naver.com>
2024-03-03 22:17:58 +0900

    청소

--- src/ch01/Ch02.java
+++ src/ch01/Ch02.java
@@ -4,34 +4,31 @@ import java.util.*;
 
 class Ch02 {
 
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
 
     public int[] solution(int[][] board, int k) {
         int[] answer = new int[2];
-        int count = 0;
+        int cnt = 0;
+        int x = 0, y = 0, d = 0;
 
-        int i = 1, x = 0, y = 0;
+        while (cnt < k) {
+            cnt++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
-        while (count < k) {
-            count++;
-
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-
-            if(nx>=board.length||nx<0||ny>=board.length||ny<0||board[nx][ny]==1){
-                i=(i+1)%4;
+            if (nx < 0 || nx >= board.length || ny < 0 || ny >= board.length || board[nx][ny] == 1) {
+                if (d == 3) d = 0;
+                else d++;
                 continue;
             }
 
-            x=nx;
-            y=ny;
+            x = nx;
+            y = ny;
         }
 
-        answer[0]=x;
-        answer[1]=y;
-
+        answer[0] = x;
+        answer[1] = y;
         return answer;
     }
 
