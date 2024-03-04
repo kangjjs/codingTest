309ee4c27cd9d48e60c8dcd9123693adebc4390b
kangjjs <emdtod12@naver.com>
2024-03-04 23:50:31 +0900

    최대길이 바이토닉 수열

--- src/ch01/Ch05.java
+++ src/ch01/Ch05.java
@@ -4,37 +4,37 @@ import java.util.ArrayList;
 
 class Ch05 {
     public int solution(int[] nums) {
-        int answer = 0;
-        int n = nums.length;
-        ArrayList<Integer> peak = new ArrayList<>();
+        int answer = 0, n = nums.length;
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
         for (int i = 1; i < n - 1; i++) {
-            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
-                peak.add(i);
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
+                arrayList.add(i);
+            }
         }
 
-        for (int t : peak) {
-            int count = 1;
-            int lt = t;
-            int rt = t;
+        for (int x : arrayList) {
+            int max = 3;
+            int lt = x - 1;
+            int rt = x + 1;
 
             while (lt > 0) {
                 if (nums[lt] > nums[lt - 1]) {
+                    max++;
                     lt--;
-                    count++;
                 } else
                     break;
             }
 
             while (rt < n - 1) {
                 if (nums[rt] > nums[rt + 1]) {
+                    max++;
                     rt++;
-                    count++;
                 } else
                     break;
             }
 
-            answer = Math.max(answer, count);
+            answer = Math.max(answer, max);
         }
 
         return answer;
@@ -42,7 +42,7 @@ class Ch05 {
 
     public static void main(String[] args) {
         Ch05 T = new Ch05();
-        System.out.println(T.solution(new int[]{1, 2, 1, 2, 3, 2, 1}));
+        System.out.println(T.solution(new int[]{1, 3, 2, 5, 7, 4, 2, 5, 1}));
         System.out.println(T.solution(new int[]{1, 1, 2, 3, 5, 7, 4, 3, 1, 2}));
         System.out.println(T.solution(new int[]{3, 2, 1, 3, 2, 4, 6, 7, 3, 1}));
         System.out.println(T.solution(new int[]{1, 3, 1, 2, 1, 5, 3, 2, 1, 1}));
