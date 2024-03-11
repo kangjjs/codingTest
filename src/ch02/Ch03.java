54886da13b2d7ed275168d99f55a644c8244437b
kangjjs <emdtod12@naver.com>
2024-03-11 18:00:54 +0900

    서로 다른 빈도수 만들기

--- src/ch02/Ch03.java
+++ src/ch02/Ch03.java
@@ -1,34 +1,32 @@
 package ch02;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
 class Ch03 {
-    public int solution(String s){
+    public int solution(String s) {
         int answer = 0;
-
-        HashMap<Character,Integer> hm =new HashMap<>();
-        HashSet<Integer> ch =new HashSet<>();
-
-        for(char x:s.toCharArray()){
-            hm.put(x,hm.getOrDefault(x,0)+1);
+        HashMap<Character, Integer> hm = new HashMap<>();
+        HashSet<Integer> hs = new HashSet<>();
+        for (char x : s.toCharArray()) {
+            hm.put(x, hm.getOrDefault(x, 0) + 1);
         }
 
-        for(char key: hm.keySet()){
-            while(ch.contains(hm.get(key))){
+        for(char key:hm.keySet()){
+            while(hs.contains(hm.get(key))){
                 answer++;
                 hm.put(key,hm.get(key)-1);
             }
-
             if(hm.get(key)==0)
                 continue;
-            ch.add(hm.get(key));
+            hs.add(hm.get(key));
         }
 
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Ch03 T = new Ch03();
         System.out.println(T.solution("aaabbbcc"));
         System.out.println(T.solution("aaabbc"));
