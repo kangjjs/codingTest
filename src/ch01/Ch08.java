961f3e95afa825aa66be0c1c283225b6082a248e
kangjjs <emdtod12@naver.com>
2024-03-11 15:31:55 +0900

    회의실 반납

--- src/ch01/Ch08.java
+++ src/ch01/Ch08.java
@@ -1,40 +1,35 @@
 package ch01;
 
 import java.util.*;
-
 class Ch08 {
     public int[] solution(int[] enter, int[] exit) {
-        int[] answer = {};
         int n = enter.length;
-
+        int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
             enter[i]--;
             exit[i]--;
         }
 
         int[] enterIdx = new int[n];
-
         for (int i = 0; i < n; i++) {
             enterIdx[enter[i]] = i;
         }
 
         int[] enterT = new int[n];
         int[] exitT = new int[n];
+        int cnt = 0;
 
-        int cnt=0;
-
-        for(int i=0,j=0;i<n;i++){
-            while(j<n && j <= enterIdx[exit[i]]){
-                enterT[enter[j]]=cnt++;
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j < n && j <= enterIdx[exit[i]]) {
+                enterT[enter[j]] = cnt++;
                 j++;
             }
-
             exitT[exit[i]] = cnt++;
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(!(exitT[i]<enterT[j] || exitT[j] < enterT[i])){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (!(exitT[i] < enterT[j] || exitT[j] < enterT[i])) {
                     answer[i]++;
                     answer[j]++;
                 }
@@ -46,7 +41,7 @@ class Ch08 {
 
     public static void main(String[] args) {
         Ch08 T = new Ch08();
-//        System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3})));
+        System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 5, 3, 4}, new int[]{2, 3, 1, 4, 5})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 3, 2, 4, 5, 7, 6, 8}, new int[]{2, 3, 5, 6, 1, 4, 8, 7})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 4, 7, 2, 3, 5, 6}, new int[]{5, 2, 6, 1, 7, 3, 4})));
