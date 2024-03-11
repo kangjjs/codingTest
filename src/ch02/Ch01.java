adde01897ca550931e8ca036ed7f3ace374f79d8
kangjjs <emdtod12@naver.com>
2024-03-11 16:00:41 +0900

    한 번 사용한 최초문자

--- src/ch02/Ch01.java
+++ src/ch02/Ch01.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 
 class Ch01 {
     public int solution(String s){
-        int answer = 0;
-
-        HashMap<Character,Integer> hm =new HashMap<>();
-
-        for(char x:s.toCharArray()){
-            hm.put(x,hm.getOrDefault(x,0)+1);
+        int answer = -1;
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char c:s.toCharArray()){
+            hm.put(c,hm.getOrDefault(c,0)+1);
         }
 
         for(int i=0;i<s.length();i++){
@@ -17,7 +15,7 @@ class Ch01 {
                 return i+1;
         }
 
-        return -1;
+        return answer;
     }
 
     public static void main(String[] args){
