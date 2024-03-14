9609932d2f0049ecae2dc478938af8a1bfc60560
kangjjs <emdtod12@naver.com>
2024-03-14 12:27:23 +0900

    경고 메일

--- src/ch02/Ch07.java
+++ src/ch02/Ch07.java
@@ -4,45 +4,38 @@ import java.util.*;
 
 class Ch07 {
 
-    public int getTime(String times){
-        int h = Integer.parseInt(times.split(":")[0]);
-        int m = Integer.parseInt(times.split(":")[1]);
-
-        return 60*h+m;
+    public int time(String time) {
+        int h = Integer.parseInt(time.split(":")[0]);
+        int m = Integer.parseInt(time.split(":")[1]);
+        return h * 60 + m;
     }
 
-    public String[] solution(String[] reports, int time){
-        String[] answer;
+    public String[] solution(String[] reports, int time) {
+        ArrayList<String> answer = new ArrayList<>();
+        HashMap<String, Integer> inT = new HashMap<>();
+        HashMap<String, Integer> sumT = new HashMap<>();
 
-        HashMap<String,Integer> inTime = new HashMap<>();
-        HashMap<String,Integer> sumTime = new HashMap<>();
-
-        for(String x : reports){
-            String name = x.split(" ")[0];
-            int t = getTime(x.split(" ")[1]);
-            String state = x.split(" ")[2];
-
-            if(state.equals("in"))
-                inTime.put(name,t);
+        for(String x:reports){
+            String a = x.split(" ")[0];
+            String b = x.split(" ")[1];
+            String c = x.split(" ")[2];
+            if(c.equals("in"))
+                inT.put(a, time(b));
             else
-                sumTime.put(name,sumTime.getOrDefault(name,0)+(t-inTime.get(name)));
+                sumT.put(a,sumT.getOrDefault(a,0)+(time(b)-inT.get(a)));
         }
 
-        ArrayList<String> arrayList = new ArrayList<>();
-
-        for(String x : sumTime.keySet()){
-            if(sumTime.get(x)>time)
-                arrayList.add(x);
+        for(String name:sumT.keySet()){
+            if(sumT.get(name)>time)
+                answer.add(name);
         }
 
-        arrayList.sort(String::compareTo);
+        Collections.sort(answer);
 
-        answer=arrayList.toArray(new String[0]);
-
-        return answer;
+        return answer.toArray(new String[0]);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Ch07 T = new Ch07();
         System.out.println(Arrays.toString(T.solution(new String[]{"john 09:30 in", "daniel 10:05 in", "john 10:15 out", "luis 11:57 in", "john 12:03 in", "john 12:20 out", "luis 12:35 out", "daniel 15:05 out"}, 60)));
         System.out.println(Arrays.toString(T.solution(new String[]{"bill 09:30 in", "daniel 10:00 in", "bill 11:15 out", "luis 11:57 in", "john 12:03 in", "john 12:20 out", "luis 14:35 out", "daniel 14:55 out"}, 120)));
