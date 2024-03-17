9cfb6d6afdc1b1ada6aff7a537c0bdd6b6eaae60
kangjjs <emdtod12@naver.com>
2024-03-17 17:40:13 +0900

    최대 길이 연속수열

--- src/ch03/Ch01.java
+++ src/ch03/Ch01.java
@@ -1,35 +1,29 @@
 package ch03;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 class Ch01 {
-    public int solution(int[] nums){
+    public int solution(int[] nums) {
         int answer = 0;
-
-        HashSet<Integer> hashSet = new HashSet<>();
-        int n= nums.length;
-        Arrays.sort(nums);
-
-        for(int x: nums){
-            hashSet.add(x);
-        }
-
-        for(int x:hashSet){
-            if(hashSet.contains(x-1))
+        HashSet<Integer> set = new HashSet<>();
+        for(int x:nums)
+            set.add(x);
+        for(int x:set){
+            if(set.contains(x-1))
                 continue;
-            int cnt=0;
-            while(hashSet.contains(x)){
+            int cnt =0;
+            while(set.contains(x)){
                 cnt++;
                 x++;
             }
-            answer=Math.max(cnt,answer);
+
+            answer = Math.max(cnt,answer);
         }
 
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Ch01 T = new Ch01();
         System.out.println(T.solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
         System.out.println(T.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
