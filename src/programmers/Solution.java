cc155cab346a0fb272af3564954a88531bbb4f83
kangjjs <emdtod12@naver.com>
2024-04-15 14:55:17 +0900

    카카오 가장 많이 받은 선물

---
+++ src/programmers/Solution.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (int i = 0; i < friends.length; i++) {
+            hm.put(friends[i], i);
+        }
+        int[] giftCnt = new int[friends.length];
+        int[][] present = new int[friends.length][friends.length];
+
+        for (String gift : gifts) {
+            String x = gift.split(" ")[0];
+            String y = gift.split(" ")[1];
+            giftCnt[hm.get(x)]++;
+            giftCnt[hm.get(y)]--;
+            present[hm.get(x)][hm.get(y)]++;
+        }
+
+        for (int i = 0; i < friends.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < friends.length; j++) {
+                if(i==j)
+                    continue;
+                if(present[i][j] > present[j][i])
+                    cnt++;
+                else if(present[i][j]==present[j][i]&&giftCnt[i]>giftCnt[j])
+                    cnt++;
+            }
+
+            answer = Math.max(answer,cnt);
+        }
+
+        return answer;
+    }
+}
