71867cd2d0386885fe014cb2a45e58026fb79e44
kangjjs <emdtod12@naver.com>
2024-04-27 16:30:58 +0900

    네트워크

---
+++ src/programmers/Network.java
@@ -0,0 +1,29 @@
+package programmers;
+
+public class Network {
+    static boolean[] visited;
+
+    static void dfs(int i,int[][] computers,int n){
+        visited[i] = true;
+
+        for(int j=0;j<n;j++){
+            if(!visited[j]&&computers[i][j]==1){
+                dfs(j,computers,n);
+            }
+        }
+    }
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+
+        for(int i=0; i<n;i++){
+            if(!visited[i]){
+                answer++;
+                dfs(i,computers,n);
+            }
+        }
+
+        return answer;
+    }
+}
