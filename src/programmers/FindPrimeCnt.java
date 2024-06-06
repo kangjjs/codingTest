2c01619c0a64f77a11398f8188e498b28afedead
kangjjs <emdtod12@naver.com>
2024-06-06 17:38:22 +0900

    k진수에서 소수 개수 구하기

---
+++ src/programmers/FindPrimeCnt.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPrimeCnt {
+    public static boolean isPrime(long checkNum) {
+        if (checkNum == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(checkNum); i++) {
+            if (checkNum % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int solution(int n, int k) {
+        int answer = 0;
+        String binary = Integer.toString(n, k);
+        List<String> nums = new ArrayList<>(List.of(binary.split("0")));
+
+        for (String str : nums) {
+            if (str.equals("")) {
+                continue;
+            }
+            long checkNum = Long.parseLong(str);
+            if (isPrime(checkNum)) {
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+}
