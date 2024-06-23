fe32c554fc8b5b1b08f3b8a6e82e9666df3221e7
kangjjs <emdtod12@naver.com>
2024-06-23 17:02:35 +0900

    한 줄로 서기

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -3,11 +3,31 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] line = new int[n+1];
+        for(int i=1;i<=n;i++){
+            int counts = Integer.parseInt(st.nextToken());
+            for(int j=1;j<=n;j++){
+                if(counts==0){
+                    if(line[j]==0){
+                        line[j]=i;
+                        break;
+                    }
+                }else if(line[j]==0){
+                    counts--;
+                }
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            System.out.print(line[i]+" ");
+        }
     }
 }
