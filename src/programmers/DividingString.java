b953cdb83079c07972ae3caff5356f0bc45eb8d1
kangjjs <emdtod12@naver.com>
2024-06-24 12:37:53 +0900

    문자열 나누기

---
+++ src/programmers/DividingString.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class DividingString {
+    public int solution(String s) {
+        int answer = 1;
+        char x = s.charAt(0);
+        int cnt =1;
+        for(int i=1;i<s.length();i++){
+            if(cnt==0){
+                answer++;
+                x=s.charAt(i);
+            }
+
+            if(x==s.charAt(i)){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+
+        return answer;
+    }
+}
