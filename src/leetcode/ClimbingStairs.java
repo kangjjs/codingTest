940d5a7e86141597288f677b7737a7e3ba922431
kangjjs <emdtod12@naver.com>
2024-10-16 23:02:14 +0900

    Climbing Stairs

---
+++ src/leetcode/ClimbingStairs.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class ClimbingStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+}
