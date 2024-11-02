6d2d9311734556efba93f4c7a7f66097e0b7523c
kangjjs <emdtod12@naver.com>
2024-11-02 12:48:51 +0900

    줄세우기

---
+++ src/backjoon/B2631.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2631 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max,dp[i]);
+                }
+            }
+        }
+
+        System.out.println(n - max);
+    }
+}
