654fc6f3f928e5ffba9c65fbcb90f33fd32f66d1
kangjjs <emdtod12@naver.com>
2024-11-12 17:44:54 +0900

    연속된 수의 합

---
+++ src/programmers/ContinuousNumberSum.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class ContinuousNumberSum {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+
+        int start = (total - (num * (num - 1)) / 2) / num;
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = start + i;
+        }
+
+        return answer;
+    }
+}
