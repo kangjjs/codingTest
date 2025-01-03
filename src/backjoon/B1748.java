8a83c93b1ab06aba2e7a8ee5ccba14761e1e87ce
강정수 <kang@gangjeongsuui-MacBookAir.local>
2025-01-03 01:13:20 -0800

    수 이어쓰기 1

---
+++ src/backjoon/B1748.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 1;
+        int ans = 0;
+        int length = 10;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == length) {
+                cnt++;
+                length = length * 10;
+            }
+            ans += cnt;
+        }
+
+        System.out.print(ans);
+    }
+}
