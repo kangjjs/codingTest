513b254f07644603531ccd96e3fe30ee5150c13f
강정수 <emdtod12@naver.com>
2025-03-09 00:43:34 -0800

    이건 꼭 풀어야해

---
+++ src/backjoon/B17390.java
@@ -0,0 +1,40 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B17390 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int[] sum = new int[n+1];
+
+		for (int i = 1; i <= n; i++) {
+			sum[i] = sum[i - 1] + arr[i - 1];
+		}
+
+		StringBuilder sb = new StringBuilder();
+		while (q-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(sum[end]-sum[start-1]).append('\n');
+		}
+
+		System.out.print(sb);
+	}
+}
