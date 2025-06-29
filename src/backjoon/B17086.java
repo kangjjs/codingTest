b04710ab1d8612cc0e27d1452daba0dc1f8e6d23
강정수 <emdtod12@naver.com>
2025-06-28 19:34:23 -0700

    아기 상어2

---
+++ src/backjoon/B17086.java
@@ -0,0 +1,63 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B17086 {
+
+    static int n, m;
+    static int[][] map, dist;
+    static int[] dx = {1, 1, 1, 0, 0, -1, -1, -1};
+    static int[] dy = {1, 0, -1, 1, -1, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        dist = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    q.add(new int[]{i, j});
+                    dist[i][j] = 1;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && dist[nx][ny] == 0) {
+                    dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans = Math.max(ans,dist[i][j]);
+            }
+        }
+
+        System.out.println(ans-1);
+    }
+}
