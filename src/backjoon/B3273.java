aad6431b3b2f9680c4f963e42b36f187b81ab0dc
강정수 <emdtod12@naver.com>
2025-08-05 05:53:49 -0700

    두 수의 합

---
+++ src/backjoon/B3273.java
@@ -0,0 +1,46 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int x = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        int ans = 0;
+        int left = 0;
+        int right = n - 1;
+
+        while(left<right){
+            int sum = arr[left] + arr[right];
+
+            if(sum>x){
+                right--;
+            }else if(sum == x){
+                ans++;
+                right--;
+                left++;
+            }else{
+                left++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
