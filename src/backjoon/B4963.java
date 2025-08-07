f41267bb1ca4f1dbd76acfe8fe6a83f2a90d4653
강정수 <emdtod12@naver.com>
2025-08-06 22:44:53 -0700

    섬의 개수

---
+++ src/backjoon/B4963.java
@@ -0,0 +1,76 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B4963 {
+
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1, 1, 1, 0, 0, -1, -1, -1};
+    static int[] dy = {1, 0, -1, 1, -1, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+
+            if (n == 0 && m == 0)
+                break;
+
+            map = new int[n][m];
+            visited = new boolean[n][m];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < m; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int cnt = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        bfs(i,j);
+                        cnt++;
+                    }
+                }
+            }
+
+            sb.append(cnt).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+}
