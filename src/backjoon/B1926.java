2523190eeaec1f95dd466a1480c0fc55f4a811db
강정수 <emdtod12@naver.com>
2025-08-11 00:06:05 -0700

    그림

---
+++ src/backjoon/B1926.java
@@ -0,0 +1,74 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B1926 {
+
+    static int n, m, ans;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        ans = 0;
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        System.out.println(ans);
+    }
+
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+        int cnt = 1;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    cnt++;
+                }
+            }
+        }
+
+        ans = Math.max(ans, cnt);
+    }
+}
