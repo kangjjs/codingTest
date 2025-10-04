81ea424a0cfd2c98ebe952392170bf156f67df2c
강정수 <emdtod12@naver.com>
2025-10-04 04:03:03 -0700

    카드 합체 놀이

---
+++ src/backjoon/B15903.java
@@ -0,0 +1,40 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class B15903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Long.parseLong(st.nextToken()));
+        }
+
+        while (m-- > 0 && !pq.isEmpty()) {
+            long x = pq.poll();
+            long y = pq.poll();
+
+            long sum = x + y;
+            pq.add(sum);
+            pq.add(sum);
+        }
+
+        long ans = 0;
+        while(!pq.isEmpty()){
+            ans+=pq.poll();
+        }
+
+        System.out.println(ans);
+    }
+}
