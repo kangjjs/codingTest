91cffe0121bfe60b1384d9ef86332d7c1cb7f016
강정수 <emdtod12@naver.com>
2025-11-01 03:07:33 -0700

    5차 전직

---
+++ src/backjoon/B16112.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B16112 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        long c = 0;
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (arr[i] * c);
+            if (c < k) {
+                c++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
