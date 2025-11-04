904404174db254276c2b618d8f838298cbd5df18
강정수 <emdtod12@naver.com>
2025-11-03 21:49:24 -0800

    안테나

---
+++ src/backjoon/B18310.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B18310 {
+
+    static int n;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[(n-1)/2]);
+    }
+}
